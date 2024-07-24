1e70b39ec13d0eaf637ae48b014cbf91b3b0b63b
ik <jeongik11029@naver.com>
2024-07-24 11:30:51 +0900

    level1-2 사칙연산 기호 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
  * 1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.
  * - 양의 정수는 각각 하나씩 전달 받습니다.
  * - 양의 정수는 적합한 타입으로 선언한 변수에 저장합니다.
+ *
+ * 2. Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.
+ *  * - 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다. (`charAt(0)`)
  */
 
 public class App {
@@ -17,5 +20,7 @@ public class App {
         int firstNum = scanner.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNum = scanner.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operation = scanner.nextLine().charAt(0); // String 타입으로 받은 문자의 처음만 유효하다.
     }
 }
