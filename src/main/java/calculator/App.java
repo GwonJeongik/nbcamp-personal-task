6682f01edfcb08091e906ad8edd97aa2542f74fa
ik <jeongik11029@naver.com>
2024-07-24 12:42:13 +0900

    level1-6 78~85번째 줄에 코드 추가 -> 배열이 다 찼으면 한 칸씩 앞으로 당기고 새로운 값 마지막 인덱스에 넣어줌.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -18,19 +18,25 @@ import java.util.Scanner;
  * <p>
  * 4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.
  * - 반복문을 사용합니다. (e.g. for, while …)
- *
+ * <p>
  * 5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
- *     - 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.
- *     - 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.
+ * - 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.
+ * - 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.
+ * <p>
+ * 6. 연산 결과가 10개를 초과하는 경우 가장 먼저 저장된 결과를 삭제하고 새로운 연산 결과가 저장될 수 있도록 소스 코드를 수정합니다.
+ * - 현재 저장된 index가 마지막(9)라면 가장 먼저 저장된 결과 값이 삭제 되고 새로운 결과 값이 마지막 index에 저장될 수 있도록 구현합니다.
+ * - Hint : 결과 값들이 한칸씩 앞으로 이동되면 되지 않을까?
  */
 
 public class App {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        double[] results = new double[10];
+        double[] results = new double[1];
         int count = 0;
 
-        while (count < results.length) {
+        while (true) {
 
             System.out.print("종료를 원하면 \"exit\"를, 진행하려면 아무값이나 입력하세요: ");
             if (scanner.nextLine().equals("exit")) {
@@ -69,7 +75,18 @@ public class App {
                     System.out.println("[" + operation + "]" + "은 연산기호가 아닙니다.");
                     continue;
             }
-            results[count++] = result;
+            //count가 result.length - 1 초과면 배열을 앞으로 한 칸씩 당기고 마지막 인덱스에 새로운 값 추가 -> 가장 먼저 저장된 값은 삭제됨.
+            if (count >= results.length) {
+
+                for (int i = 0; i < results.length - 1; i++) {
+                    results[i] = results[i + 1];
+                }
+                results[results.length - 1] = result;
+
+            } else {
+                results[count++] = result;
+            }
+
             System.out.println("사칙연산 결과는: " + result + "입니다.");
         }
     }
